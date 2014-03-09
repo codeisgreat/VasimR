8ea5381c51e32ea80dfc101f8343becc4fc82b4b
Juan Zamora <juazammo@gmail.com>
2014-03-08 18:30:49 -0600

    Session V + GamePad Support
    VasimR now supports single player xbox wireless controller.
    Fire: A
    Boost: X
    (X,Y) Axis Control: Left ThumbStick

---
+++ SessionV + GamePad/VasimR/VasimR/Parallax/ScrollingPane.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace VasimR.Parallax
+{
+    /// <summary>
+    /// Represents a single backgorund tile that will be looping in the background
+    /// </summary>
+    public class ScrollingPane : IGamefied
+    {
+        public Texture2D texture;
+        private float speed;
+        private SpriteBatch spriteBatch;
+        public Vector2 Coordinates;
+        public Vector2 Size;
+
+        public float Speed { get { return speed; } set { speed = value; } }
+
+        /// <summary>
+        /// Initialize the object to move at the position and speed defined
+        /// </summary>
+        /// <param name="texture">The tile texture</param>
+        /// <param name="rectangle">the tile position</param>
+        /// <param name="speed">speed of the tile</param>
+        public ScrollingPane(Texture2D texture, Vector2 coordinates, Vector2 size, float speed)
+        {
+            this.texture = texture;
+            this.Coordinates = coordinates;
+            this.Size = size;
+            this.speed = speed;
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public void LoadContent(GraphicsDevice device, SpriteBatch spriteBatch)
+        {
+            // in this case, textures should be already loaded on the parallax manager.
+            this.spriteBatch = spriteBatch;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // rectangle x position will be defined by the speed parameter
+            //this.rectangle.X -= speed;
+            this.Coordinates = new Vector2(this.Coordinates.X - speed, this.Coordinates.Y);
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            // draw the image on the "rectangle" position with a white background colors
+            //this.spriteBatch.Draw(this.texture, this.rectangle, Color.White);
+            spriteBatch.Draw(this.texture, this.Coordinates, null, Color.White * 1.0f,
+                0f, new Vector2(0, 0), 1f, SpriteEffects.None, 1f);
+        }
+    }
+}
---
+++ SessionV + GamePad/VasimR/VasimR/Spaceship/RocketManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shooting2D;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using VasimR.Common;
+using ProjectMercury.Renderers;
+using ProjectMercury;
+using Microsoft.Xna.Framework.Input;
+
+namespace VasimR.Spaceship
+{
+    /// <summary>
+    /// This class is a proxy for the Game1 class and setup the rocket on the screen
+    /// </summary>
+    public class RocketManager : IGamefied
+    {
+        public FlyingObject Rocket { get; set; }
+        SpriteBatch spriteBatch;
+        Texture2D shipSprite;
+        Texture2D bulletSprite;
+        ContentManager content;
+        GraphicsDeviceManager graphics;
+
+        public RocketManager(ContentManager content, GraphicsDeviceManager graphics)
+        {
+            this.content = content;
+            this.graphics = graphics;
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public void LoadContent(Microsoft.Xna.Framework.Graphics.GraphicsDevice
+            device, Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            this.spriteBatch = spriteBatch;
+            bulletSprite = content.Load<Texture2D>("Images/spritesSpace");
+            bulletSprite = Util.Crop(bulletSprite, new Rectangle(32 * 5, 32 * 7, 32, 32));
+            Texture2D sprites = content.Load<Texture2D>("Images/spritesSpace");
+            shipSprite = Util.Crop(sprites, new Rectangle(128, 64, 32, 32));
+
+            // new Vector2(64, 29) is the size of the sprite element (single image)
+            // new Vector2(150, 230) is the ship startup position on the screen
+            // 14 * 3 is the frames per second
+            // 12 is the rocket movement speed (play with this)
+            Rocket = new Rocket(shipSprite, bulletSprite, 1, new Vector2(shipSprite.Width, shipSprite.Height / 2),
+                new Vector2(150, 230), 1, 12);
+
+           
+        }
+
+        public void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            Rocket.Update(gameTime);
+        }
+
+        public void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            Rocket.Draw(this.spriteBatch);
+        }
+
+        public BoundingSphere GetCollissionBox()
+        {
+            return Rocket.GetCollissionBox();
+        }
+
+    }
+}
